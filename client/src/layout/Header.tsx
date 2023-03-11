afefc966076d14a025c8505c407d997ad52c4569
Yoriqulov Oybek <hhoybek@gmail.com>
2023-03-11 09:26:08 +0500

    header profile bug fixed

--- client/src/layout/Header.tsx
+++ client/src/layout/Header.tsx
@@ -19,14 +19,14 @@ import { useAppContext } from '@/context/app.context'
 import { routeNames } from '@/common/route-names'
 import LogoutIcon from '@mui/icons-material/Logout'
 import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
-import Delayed from '@/components/Delayed'
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'
 
 const Header = () => {
   const anchorRef = useRef(null)
   const [open, setOpen] = useState<boolean>(false)
   const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
   const { pathname, reload } = useRouter()
-  const { token, destroyToken, user } = useAppContext()
+  const { destroyToken, user } = useAppContext()
   const handleLogout = () => {
     destroyToken()
     reload()
@@ -58,128 +58,133 @@ const Header = () => {
         >
           Discounts
         </Typography>
-        <Delayed waitBeforeShow={100}>
-          <Grid sx={{ display: 'flex' }}>
-            {token && user ? (
-              <Fragment>
-                <Button
-                  onMouseLeave={handleClose}
-                  ref={anchorRef}
-                  onMouseOver={handleClick}
-                  onClick={handleClick}
-                  sx={{
-                    textTransform: 'none',
-                    color: 'white',
-                    marginRight: '10px'
-                  }}
-                  size="small"
-                  color="inherit"
-                  variant="outlined"
+        <Grid sx={{ display: 'flex' }}>
+          <Fragment>
+            <Button
+              onMouseLeave={handleClose}
+              ref={anchorRef}
+              onMouseOver={handleClick}
+              onClick={handleClick}
+              startIcon={<AccountCircleIcon />}
+              sx={{
+                textTransform: 'none',
+                color: 'white',
+                marginRight: '10px'
+              }}
+              size="small"
+              color="inherit"
+              variant="outlined"
+            >
+              {user?.username || 'Account'}
+            </Button>
+            <Popper
+              anchorEl={anchorEl}
+              style={{ zIndex: 10000 }}
+              placement="bottom"
+              open={open}
+              role={undefined}
+              transition
+            >
+              {({ TransitionProps }) => (
+                <Zoom
+                  timeout={250}
+                  {...TransitionProps}
+                  style={{ transformOrigin: 'center top' }}
                 >
-                  {user?.username}
-                </Button>
-                <Popper
-                  anchorEl={anchorEl}
-                  style={{ zIndex: 10000 }}
-                  placement="bottom"
-                  open={open}
-                  role={undefined}
-                  transition
-                >
-                  {({ TransitionProps }) => (
-                    <Zoom
-                      timeout={250}
-                      {...TransitionProps}
-                      style={{ transformOrigin: 'center top' }}
-                    >
-                      <Paper
-                        variant="elevation"
-                        sx={{
-                          padding: '10px',
-                          marginTop: '5px'
-                        }}
-                        elevation={8}
-                        onMouseLeave={handleClose}
-                      >
-                        <ClickAwayListener onClickAway={handleClose}>
-                          <MenuList
-                            onMouseEnter={handleClick}
-                            autoFocusItem={open}
-                            sx={{ display: 'grid', rowGap: '10px' }}
+                  <Paper
+                    variant="elevation"
+                    sx={{
+                      padding: '10px',
+                      marginTop: '5px'
+                    }}
+                    elevation={8}
+                    onMouseLeave={handleClose}
+                  >
+                    <ClickAwayListener onClickAway={handleClose}>
+                      {user ? (
+                        <MenuList
+                          onMouseEnter={handleClick}
+                          autoFocusItem={open}
+                          sx={{ display: 'grid', rowGap: '10px' }}
+                        >
+                          <Button
+                            href={routeNames.profile}
+                            component={Link}
+                            onClick={handleClose}
+                            startIcon={<PersonOutlineIcon />}
+                            sx={{
+                              textTransform: 'none'
+                            }}
+                            size="small"
+                            color="success"
+                            variant={
+                              pathname === routeNames.profile
+                                ? 'contained'
+                                : 'outlined'
+                            }
                           >
-                            <Button
-                              href={routeNames.profile}
-                              component={Link}
-                              onClick={handleClose}
-                              startIcon={<PersonOutlineIcon />}
-                              sx={{
-                                textTransform: 'none'
-                              }}
-                              size="small"
-                              color="success"
-                              variant={
-                                pathname === routeNames.profile
-                                  ? 'contained'
-                                  : 'outlined'
-                              }
-                            >
-                              Profile
-                            </Button>
-                            <Button
-                              onClick={handleLogout}
-                              startIcon={<LogoutIcon />}
-                              sx={{
-                                textTransform: 'none'
-                              }}
-                              size="small"
-                              color="error"
-                              variant="outlined"
-                            >
-                              Logout
-                            </Button>
-                          </MenuList>
-                        </ClickAwayListener>
-                      </Paper>
-                    </Zoom>
-                  )}
-                </Popper>
-              </Fragment>
-            ) : (
-              <Fragment>
-                <Button
-                  component={Link}
-                  startIcon={<LoginIcon />}
-                  href={routeNames.signIn}
-                  sx={{
-                    textTransform: 'none',
-                    color: 'white',
-                    marginRight: '10px'
-                  }}
-                  size="small"
-                  color={pathname === routeNames.signIn ? 'warning' : 'inherit'}
-                  variant={
-                    pathname === routeNames.signIn ? 'contained' : 'outlined'
-                  }
-                >
-                  Sign in
-                </Button>
-                <Button
-                  component={Link}
-                  startIcon={<PersonAddAltIcon />}
-                  href={routeNames.signUp}
-                  sx={{ textTransform: 'none' }}
-                  size="small"
-                  color={pathname === routeNames.signUp ? 'warning' : 'inherit'}
-                  variant={
-                    pathname === routeNames.signUp ? 'contained' : 'outlined'
-                  }
-                >
-                  Sign up
-                </Button>
-              </Fragment>
-            )}
-          </Grid>
-        </Delayed>
+                            Profile
+                          </Button>
+                          <Button
+                            onClick={handleLogout}
+                            startIcon={<LogoutIcon />}
+                            sx={{
+                              textTransform: 'none'
+                            }}
+                            size="small"
+                            color="error"
+                            variant="outlined"
+                          >
+                            Logout
+                          </Button>
+                        </MenuList>
+                      ) : (
+                        <MenuList
+                          onMouseEnter={handleClick}
+                          autoFocusItem={open}
+                          sx={{ display: 'grid', rowGap: '10px' }}
+                        >
+                          <Button
+                            component={Link}
+                            startIcon={<LoginIcon />}
+                            href={routeNames.signIn}
+                            sx={{
+                              textTransform: 'none'
+                            }}
+                            size="small"
+                            color="success"
+                            variant={
+                              pathname === routeNames.signIn
+                                ? 'contained'
+                                : 'outlined'
+                            }
+                          >
+                            Sign in
+                          </Button>
+                          <Button
+                            component={Link}
+                            startIcon={<PersonAddAltIcon />}
+                            href={routeNames.signUp}
+                            sx={{ textTransform: 'none' }}
+                            size="small"
+                            color="warning"
+                            variant={
+                              pathname === routeNames.signUp
+                                ? 'contained'
+                                : 'outlined'
+                            }
+                          >
+                            Sign up
+                          </Button>
+                        </MenuList>
+                      )}
+                    </ClickAwayListener>
+                  </Paper>
+                </Zoom>
+              )}
+            </Popper>
+          </Fragment>
+        </Grid>
       </Toolbar>
     </AppBar>
   )
--- client/src/pages/profile.tsx
+++ client/src/pages/profile.tsx
@@ -2,11 +2,8 @@ import React from 'react'
 import { Grid, Typography } from '@mui/material'
 import { withLayout } from '@/layout'
 import PageHead from '@/components/PageHead'
-import { useAppContext } from '@/context/app.context'
 
 const Profile = () => {
-  const { user } = useAppContext()
-  console.log(user)
   return (
     <>
       <PageHead title="Profile" />
